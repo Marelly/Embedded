05abeb5411b39f6b874f78aec2584feb68182590
Marelly <ramimarelly"gmail.com>
2024-09-06 08:35:12 +0300

    Added Image button

--- GameBasic/src/my_ui_elements/MusicButton.java
+++ GameBasic/src/my_ui_elements/MusicButton.java
@@ -2,12 +2,12 @@ package my_ui_elements;
 
 import base.Game;
 import base.AudioPlayer.MusicStatus;
-import ui_elements.GameButton;
+import ui_elements.GameImageButton;
 
-public class MusicButton extends GameButton{
+public class MusicButton extends GameImageButton{
 	
 	public MusicButton(String id, String name, int posX, int posY) {
-		super(id, name, 100, 40, posX, posY);
+		super(id, name, 70, 60, posX, posY, "resources/play.jpg");
 	}
 
 	@Override
@@ -17,12 +17,14 @@ public class MusicButton extends GameButton{
 		super.action();
 		if (Game.audioPlayer().getStatus() == MusicStatus.STOPPED) {
 			Game.audioPlayer().play("resources/audio/audio_sample.wav", 0);
-			this.setText("Stop");
+			this.setImage("resources/stop.jpg");
+			//this.setText("Stop");
 			Game.UI().canvas().setBackgroundImage("resources/background2.jpg");
 		}
 		else {
 			Game.audioPlayer().stop();
-			this.setText("Play");
+			this.setImage("resources/play.jpg");
+			//this.setText("Play");
 			Game.UI().canvas().setBackgroundImage("resources/background1.jpg");
 		}
 	}
