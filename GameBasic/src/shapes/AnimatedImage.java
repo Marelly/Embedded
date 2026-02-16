c888a714be26c84b22a4d389d0c52d705ccd8161
Marelly <ramimarelly"gmail.com>
2026-02-16 16:18:51 +0200

    leftovers

---
+++ GameBasic/src/shapes/AnimatedImage.java
@@ -0,0 +1,98 @@
+package shapes;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
+import java.io.File;
+import java.io.IOException;
+
+public class AnimatedImage extends Shape {
+    private BufferedImage spriteSheet;
+    private String currentSpritePath;
+    private int frameWidth, frameHeight;
+    private int totalFrames;
+    private int currentFrame = 0;
+    private boolean isMirrored; // For image mirroring
+    private int posX, posY;
+
+    public AnimatedImage(String id, int frameWidth, int frameHeight, boolean isMirrored) {
+        super(id);
+        this.frameWidth = frameWidth;
+        this.frameHeight = frameHeight;
+        this.isMirrored = isMirrored;
+        this.setDraggable(false);
+        // Default settings
+        this.posX = 0;
+        this.posY = 0;        
+    }
+
+    public void setSpriteSheet(String spritePath, int totalFrames) {
+        if (!spritePath.equals(currentSpritePath)) { // Avoid reloading the same sprite sheet
+            try {
+                this.spriteSheet = ImageIO.read(new File(spritePath));
+                this.currentSpritePath = spritePath;
+                this.totalFrames = totalFrames;
+                this.currentFrame = 0; // Reset animation
+                
+            } catch (IOException e) {
+                e.printStackTrace();
+                throw new RuntimeException("Failed to load sprite sheet: " + spritePath);
+            }
+        }
+    }
+
+    public void setIsMirrored(boolean isMirrored){
+        this.isMirrored = isMirrored;
+    }
+    
+    public int getPosX() {
+        return posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void nextFrame() {
+        if (totalFrames > 0) {
+            currentFrame = (currentFrame + 1) % totalFrames; // Loop animation
+        }
+    }
+
+    public int getCurrentFrame(){
+        return currentFrame;
+    }
+
+    @Override
+    public void draw(Graphics2D g) {
+        int srcX = currentFrame * frameWidth;
+        int srcY = 0; // Using 1-d spritesheets
+
+        if (isMirrored) {
+            // Flip horizontally
+            g.drawImage(spriteSheet, posX + frameWidth, posY, posX, posY + frameHeight, 
+                        srcX, srcY, srcX + frameWidth, srcY + frameHeight, null);
+        } else {
+            g.drawImage(spriteSheet, posX, posY, posX + frameWidth, posY + frameHeight, 
+                        srcX, srcY, srcX + frameWidth, srcY + frameHeight, null);
+        }
+    }
+
+    @Override
+    public void move(int dx, int dy) {
+        this.posX += dx;
+        this.posY += dy;
+    }
+
+    @Override
+    public void moveToLocation(int x, int y) {
+        this.posX = x;
+        this.posY = y;
+    }
+
+    @Override
+    public boolean isInArea(int x, int y) {
+        return (x >= posX && x <= posX + frameWidth) && (y >= posY && y <= posY + frameHeight);
+    }
+}
